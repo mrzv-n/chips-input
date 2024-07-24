0b7ec411548bae2eb6075f276a3877586a132f8d
morozovnn <mrzv.n@yandex.ru>
2024-07-25 02:23:04 +0300

    Add chip removing logic

--- src/components/UI/ChipsInput/ChipsInput.vue
+++ src/components/UI/ChipsInput/ChipsInput.vue
@@ -7,14 +7,20 @@
       <div class="chips-input__body">
         <div class="chips-input__chips">
           <div
-            v-for="(item, index) in model"
-            :key="item + index"
+            v-for="(chipLabel, index) in model"
+            :key="chipLabel + index"
+            :ref="(chip) => addChipRef(chip, index)"
             :class="['chip', chipClassNames]"
+            role="option"
+            tabindex="0"
+            @keydown="(e) => handleChipKeyDown(e, index)"
           >
-            <div class="chip__label">{{ item }}</div>
+            <div class="chip__label">{{ chipLabel }}</div>
             <button
               v-if="!disabled"
               class="chip__remove-btn"
+              @click.stop="() => removeChip(index, chipLabel)"
+              tabindex="-1"
             >
               <CrossIcon class="chip__remove-icon" />
             </button>
@@ -27,6 +33,7 @@
             v-model="inputValue"
             @keydown="handleKeyDown"
             @blur="handleBlur"
+            @focus="handleFocus"
           />
         </div>
       </div>
@@ -59,7 +66,7 @@ const props = withDefaults(defineProps<ChipsInputProps>(), {
   max: Infinity,
   addOnBlur: true,
   allowDuplicate: true,
-  disabled: true,
+  disabled: false,
   validator: () => true,
   hint: '',
   separators: () => [',', ' ', '\n']
@@ -71,6 +78,7 @@ const chips = ref([...props.model])
 const error = ref(false)
 const inputField = ref<HTMLInputElement>()
 const inputValue = ref('')
+let chipRefs: HTMLElement[] = []
 const rootClassNames = computed(() => ({
   _error: error.value,
   _disabled: props.disabled,
@@ -80,6 +88,10 @@ const chipClassNames = computed(() => ({
   _error: error.value,
   _disabled: props.disabled
 }))
+
+function addChipRef(chipRef: HTMLElement, index: number) {
+  chipRefs[index] = chipRef
+}
 
 function focusInput(): void {
   inputField.value!.focus()
@@ -94,6 +106,31 @@ function handleKeyDown(e: KeyboardEvent): void {
       clearInputValue()
     }
   }
+
+  if (isInputCaretOnStart() && e.key === 'ArrowLeft' && chips.value.length) {
+    const lastChipIndex = chips.value.length - 1
+
+    chipRefs[lastChipIndex].focus()
+  }
+}
+
+function handleChipKeyDown(e: KeyboardEvent, index: number) {
+  let currentIndex = index
+
+  if (e.key === 'ArrowLeft' && currentIndex > 0) {
+    chipRefs[--currentIndex].focus()
+  }
+  if (e.key === 'ArrowRight') {
+    currentIndex < chips.value.length - 1 ? chipRefs[++currentIndex].focus() : focusInput()
+  }
+
+  if (e.key === 'Backspace') {
+    removeChip(currentIndex, chips.value[currentIndex])
+  }
+}
+
+function handleFocus(): void {
+  emit('focus')
 }
 
 function handleBlur(): void {
@@ -101,6 +138,8 @@ function handleBlur(): void {
     addChip(inputValue.value)
     clearInputValue()
   }
+
+  emit('blur')
 }
 
 function clearInputValue(): void {
@@ -115,11 +154,24 @@ function isInputCaretOnEnd(): boolean {
   return inputField.value!.selectionStart === inputValue.value!.length
 }
 
-function addChip(chipValue: string): void {
-  if (!props.allowDuplicate && chips.value.includes(chipValue)) return
+function addChip(chipLabel: string): void {
+  if (!props.allowDuplicate && chips.value.includes(chipLabel)) return
 
-  chips.value.push(chipValue)
+  chips.value.push(chipLabel)
+  emit('add', chipLabel)
   emit('update:model', chips.value)
+}
+
+function removeChip(index: number, chipLabel: string) {
+  // Реализовано через slice и в emit('remove') передается объект, так как возможны одноименные чипсы
+  chips.value = [...chips.value.slice(0, index), ...chips.value.slice(index + 1)]
+
+  emit('remove', {
+    index,
+    chipLabel
+  })
+  emit('update:model', chips.value)
+  focusInput()
 }
 
 function isKeySeparator(key: string): boolean {
